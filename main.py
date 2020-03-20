be45283ad2ed13371caa7ac3db0a9e5fec258a74
phage <kyp0717@gmail.com>
2020-03-20 12:19:28 -0400

    add portfolio file

--- main.py
+++ main.py
@@ -6,34 +6,6 @@ import behavior as bh
 import algo
 import timeSeries as tsr
 
-## helper function to be used with pandas apply
-def linearRegress(x):
-    lr = stats.linregress(x['timepoint'].astype(int), x['price'].astype(float))
-    return lr.slope
-
-## New Function
-Portfolio: List[Tuple[ticker, algo]]
-def reconcile(p: Portfolio) -> Portfolio:
-    pos = getPosition(conn)
-
-ticker: str
-position: List[ticker]
-def getPosition(ibxConnn) -> position:
-    pass
-
-## New Function
-## This function is a side affect
-## It will execute the algo and return nothing
-## No need to define type
-def triage(p: Portfolio, conn) -> Portfolio:
-    pnlList = getPnl()
-    for tk, pnl in pnlList:
-        if pnl < -0.05:
-            sell(pk)
-    # synchronize portfolio and algo list
-    p2 = reconcile(p)
-    return p2
-
    
 market_close = datetime.datetime.now().replace(hour=16, minute=0) 
 pfl = Portfolio[]
